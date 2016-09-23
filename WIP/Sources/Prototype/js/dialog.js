39fac128759b5ce3f4e7eefe2c7b5bde5799da9b
nghiadtse03220 <nghiadtse03220@fpt.edu.vn>
2016-09-23 16:01:43 +0700

    overflow all dialog

--- WIP/Sources/Prototype/js/dialog.js
+++ WIP/Sources/Prototype/js/dialog.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
+	$("#loginLink").click(function(){
+		$('body').css('overflow-y','visible');
+	});
+
 	$("#register-box").click(function(){
-		$('body').css('overflow-y','hidden');
+		$('body').css('overflow-y','visible');
 	});
 
 	$("#forgot-box").click(function(){
-		$('body').css('overflow-y','hidden');
+		$('body').css('overflow-y','visible');
 	});
 
 	$("#backToLoginDialog").click(function(){
-		$('body').css('overflow-y','hidden');
+		$('body').css('overflow-y','visible');
 	});
 });
