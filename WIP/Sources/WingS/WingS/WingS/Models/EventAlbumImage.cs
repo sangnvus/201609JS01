6d885c69e96069fb19bc47518b185aeba9ec0b78
nhienlhse03377 <blacksnow055@gmail.com>
2016-09-29 21:18:54 +0700

    Update DB

---
+++ WIP/Sources/WingS/WingS/WingS/Models/EventAlbumImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WingS.Models
+{
+    public class EventAlbumImage
+    {
+
+        [Key]
+        public int ImageId { get; set; }
+        [ForeignKey("Event")]
+        public int EventId { get; set; }
+        public string ImgaeUrl { get; set; }
+        public bool? status { get; set; }
+        public virtual Event Event { get; set; }
+    }
+}
