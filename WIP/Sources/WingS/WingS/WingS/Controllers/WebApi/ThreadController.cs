b4cf5d5c92d6a0c5f8f4d83ec7e9b79ef629161b
nghiadtse03220 <nghiadtse03220@fpt.edu.vn>
2016-10-01 00:07:32 +0700

    add ThreadController.cs

---
+++ WIP/Sources/WingS/WingS/WingS/Controllers/WebApi/ThreadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WingS.DataAccess;
+using WingS.DataHelper;
+using WingS.Models;
+using WingS.Models.DTOs;
+
+namespace WingS.Controllers.WebApi
+{
+    public class ThreadController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetTopFourThread()
+        {
+            List<Thread> topFourThread = null;
+            var basicThreadList = new List<ThreadBasicInfo>();
+
+            using (var db = new ThreadDAL())
+            {
+                topFourThread = db.GetTopThreadByView(4);
+                foreach (Thread thread in topFourThread)
+                {
+                    basicThreadList.Add(new ThreadBasicInfo
+                    {
+                        ThreadID = thread.ThreadId,
+                        UserID = thread.UserId,
+                        ThreadName = thread.Title,
+                        ImageUrl = "",
+                        Content = thread.Content,
+                        Status = true
+                    });
+                }
+            }
+
+            return Ok(new HTTPMessageDTO {Status = WsConstant.HttpMessageType.SUCCESS, Data = basicThreadList});
+
+        }
+    }
+}
