24cb4ac4734ea341e63ab413dc059d56fcfcf8db
duytnse03267 <duytn03267@gmail.com>
2016-10-13 18:41:19 +0700

    Update UserProfile: - ProfileController

---
+++ WIP/Sources/WingS/WingS/WingS/App/Controller/ProfileController.js
@@ -0,0 +1,13 @@
+﻿app.controller("ProfileController", function ($scope, $http, $routeParams) {
+    var UserName = $routeParams.UserName;
+    $http({
+        url: "/api/User/GetCurrentUser",
+        method: "GET",
+        params: { userName: UserName },
+        contentType: "application/json",
+    }).success(function (response) {
+        $scope.UserInfo = response.Data;
+        
+    });
+
+});
