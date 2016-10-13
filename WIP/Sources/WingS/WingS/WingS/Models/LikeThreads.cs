504c0155091e1fc8a4507df9ee668b886192a6e7
nhienlhse03377 <blacksnow055@gmail.com>
2016-10-13 22:03:06 +0700

    Updates Model LikeThreads

---
+++ WIP/Sources/WingS/WingS/WingS/Models/LikeThreads.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WingS.Models
+{
+    public class LikeThreads
+    {
+        [Key]
+        public int LikeId { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        [ForeignKey("Thread")]
+        public int ThreadId { get; set; }
+        public bool Status { get; set; }
+        public virtual Ws_User User { get; set; }
+        public virtual Thread Thread { get; set; }
+    }
+}
