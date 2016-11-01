24106a0a869b4a04e214f22ae5b98e7bae1614f4
nhienlhse03377 <blacksnow055@gmail.com>
2016-11-01 16:12:25 +0700

    Update Hub

--- WIP/Sources/WingS/WingS/WingS/ChatHub/Startup.cs
+++ WIP/Sources/WingS/WingS/WingS/ChatHub/Startup.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
+
+[assembly: OwinStartup(typeof(WingS.ChatHub.Startup))]
+
 namespace WingS.ChatHub
 {
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            // Any connection or hub wire up and configuration should go here
-            app.MapSignalR();
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
         }
     }
 }
