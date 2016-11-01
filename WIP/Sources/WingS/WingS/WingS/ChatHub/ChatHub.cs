f4a9708e33ec4471768ad5696e7588ac06cf5836
nhienlhse03377 <blacksnow055@gmail.com>
2016-11-01 15:20:50 +0700

    Update ChatHub

---
+++ WIP/Sources/WingS/WingS/WingS/ChatHub/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WingS.ChatHub
+{
+    public class ChatHub : Hub
+    {
+        #region Connect
+        public void Connect(string userName)
+        {
+          
+        }
+        #endregion
+    }
+}
