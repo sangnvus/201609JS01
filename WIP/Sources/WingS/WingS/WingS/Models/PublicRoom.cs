488b3475eb51fa5e8f586381202b42462d645d6c
nhienlhse03377 <blacksnow055@gmail.com>
2016-11-02 16:44:38 +0700

    Update Database

---
+++ WIP/Sources/WingS/WingS/WingS/Models/PublicRoom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WingS.Models
+{
+    public class PublicRoom
+    {
+        [Key]
+        public int PublicRoomId { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        [ForeignKey("Event")]
+        public int EventId { get; set;}
+        public string ConnectionId { get; set; }
+        public virtual Ws_User User { get; set; }
+        public virtual Event Event { get; set; }
+    }
+}
