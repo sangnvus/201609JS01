7a192c285967299961bec5a904307a03f0891d94
nhienlhse03377 <blacksnow055@gmail.com>
2016-11-02 14:53:34 +0700

    Update Message

--- WIP/Sources/WingS/WingS/WingS/App/Controller/ProfileController.js
+++ WIP/Sources/WingS/WingS/WingS/App/Controller/ProfileController.js
@@ -9,5 +9,17 @@
         $scope.UserInfo = response.Data;
         
     });
-
+    $scope.sendMessage = function (titleMessage, content) {
+        $http({
+            url: "/api/Conservation/AddConservation",
+            method: "post",
+            data: { Title: titleMessage, Content: content, ReceiverName: $scope.UserInfo.UserName },
+            contentType: "application/json",
+        }).success(function (response) {
+            $(".modal-body").hide();
+            $(".sendMessage").hide();
+            $(".modal-message").show();
+            $(".closeForm").show();
+        });
+    };
 });
--- WIP/Sources/WingS/WingS/WingS/DataAccess/ConservationDAL.cs
+++ WIP/Sources/WingS/WingS/WingS/DataAccess/ConservationDAL.cs
@@ -18,46 +18,46 @@ namespace WingS.DataAccess
                 return returnedConservation;
             }
         }
-        //public List<MessageBasicInfoDTO> GetAllMessageByConservationId(int id)
-        //{
-        //    List<MessageBasicInfoDTO> MessageList = new List<MessageBasicInfoDTO>();
-        //    MessageBasicInfoDTO current = new MessageBasicInfoDTO();
-        //    using (var db = new Ws_DataContext())
-        //    {
-        //        var list = (from p in db.Message
-        //                    where p.ConservationId == id
-        //                    select p).ToList();
-        //        foreach(var item in list)
-        //        {
-        //            //Set time
-        //            if (DateTime.Now.Subtract(item.CreatedDate).Hours <= 24 && DateTime.Now.Subtract(item.CreatedDate).Hours >= 1)
-        //                current.CreatedDate = DateTime.Now.Subtract(item.CreatedDate).Hours + " Tiếng cách đây";
-        //            else if (DateTime.Now.Subtract(item.CreatedDate).Hours > 24)
-        //                current.CreatedDate = item.CreatedDate.ToString("H:mm:ss dd/MM/yy");
-        //            else current.CreatedDate = DateTime.Now.Subtract(item.CreatedDate).Minutes + " Phút cách đây";
-        //            //Set other atributes
-        //            current.CreatorImage = item.User.User_Information.ProfileImage;
-        //            current.CreatorName = item.User.UserName;
-        //            current.Content = item.Content;
-        //            MessageList.Add(current);
-        //        }
-        //    }
-        //    return MessageList;
-           
-        //}
-        public List<ConservationBasicInfoDTO> GetAllConservationByUserId(int UserId)
+        public List<MessageBasicInfoDTO> GetAllMessageByConservationId(int id)
+        {
+            List<MessageBasicInfoDTO> MessageList = new List<MessageBasicInfoDTO>();
+            MessageBasicInfoDTO current = new MessageBasicInfoDTO();
+            using (var db = new Ws_DataContext())
+            {
+                var list = (from p in db.Message
+                            where p.ConservationId == id
+                            select p).ToList();
+                foreach (var item in list)
+                {
+                    //Set time
+                    if (DateTime.Now.Subtract(item.CreatedDate).Hours <= 24 && DateTime.Now.Subtract(item.CreatedDate).Hours >= 1)
+                        current.CreatedDate = DateTime.Now.Subtract(item.CreatedDate).Hours + " Tiếng cách đây";
+                    else if (DateTime.Now.Subtract(item.CreatedDate).Hours > 24)
+                        current.CreatedDate = item.CreatedDate.ToString("H:mm:ss dd/MM/yy");
+                    else current.CreatedDate = DateTime.Now.Subtract(item.CreatedDate).Minutes + " Phút cách đây";
+                    //Set other atributes
+                    current.CreatorImage = item.User.User_Information.ProfileImage;
+                    current.CreatorName = item.User.UserName;
+                    current.Content = item.Content;
+                    MessageList.Add(current);
+                }
+            }
+            return MessageList;
+
+        }
+        public List<ConservationBasicInfoDTO> GetAllConservationByUserId()
         {
             List<ConservationBasicInfoDTO> ConservationList = new List<ConservationBasicInfoDTO>();
             using (var db = new Ws_DataContext())
             {
                 var list = (from p in db.Conservation
-                            where p.CreatorId == UserId || p.ReceiverId == UserId
+                            where p.CreatorId == WsConstant.CurrentUser.UserId || p.ReceiverId == WsConstant.CurrentUser.UserId
                             select p).OrderByDescending(x=>x.UpdatedTime).ToList();
                 foreach (var item in list)
                 {
                     ConservationBasicInfoDTO current = new ConservationBasicInfoDTO();
                     //Set Image
-                    if (item.ReceiverId == UserId)
+                    if (item.ReceiverId == WsConstant.CurrentUser.UserId)
                     {
                         current.AvatarUrl = item.Creator.User_Information.ProfileImage;
                         current.CreatorName = item.Creator.UserName;
@@ -75,6 +75,7 @@ namespace WingS.DataAccess
                     else current.CreatedDate = DateTime.Now.Subtract(item.UpdatedTime).Minutes + " Phút cách đây";
                     //Set other attributes
                     current.Title = item.Title;
+                    current.ConservationId = item.ConservationId;
                     ConservationList.Add(current);
                 }
                 return ConservationList;
