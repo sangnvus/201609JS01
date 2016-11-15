4ee9ea750dba13fbe6dcc4099ec0777b54d9cc13
tuanhase03108 <anhtuanvt93@gmail.com>
2016-11-15 15:31:05 +0700

    add Admin_EventCircleTile and Admin_EventList

---
+++ WIP/Sources/WingS/WingS/WingS/Controllers/WebApi/AdminEventController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WingS.DataAccess;
+using WingS.DataHelper;
+using WingS.Models;
+using WingS.Models.DTOs;
+
+namespace WingS.Controllers.WebApi
+{
+    public class AdminEventController : ApiController
+    {
+        /// <summary>
+        /// Get user manage basisc information
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IHttpActionResult GetEventManageBasicInfor()
+        {
+            try
+            {
+                EventCircleTileDTO circleInfor = new EventCircleTileDTO();
+                using (var db = new EventDAL())
+                {
+                    circleInfor = db.GetEventCircleTile();
+                }
+
+                return Ok(new HTTPMessageDTO
+                {
+                    Status = WsConstant.HttpMessageType.SUCCESS,
+                    Data = circleInfor
+                });
+            }
+            catch (Exception)
+            {
+
+                return Ok(new HTTPMessageDTO
+                {
+                    Status = WsConstant.HttpMessageType.ERROR
+                });
+            }
+        }
+    }
+}
