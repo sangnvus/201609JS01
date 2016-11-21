ede2bef5512a8bcf4adc732e80306795ad5624ed
tuanhase03108 <anhtuanvt93@gmail.com>
2016-11-21 17:11:33 +0700

    Fix Donation Page

---
+++ WIP/Sources/WingS/WingS/WingS/App/Controller/DonationController.js
@@ -0,0 +1,12 @@
+﻿app.controller("DonationController", function ($scope, $http, $routeParams, $sce) {
+    var eventId = $routeParams.EventId;
+    $http({
+        url: "/api/Event/",
+        method: "Get",
+        params: { id: eventId },
+        contentType: "application/json"
+    }).success(function (response) {
+        $scope.DonateInfo = response.Data;
+    });
+});
+
---
+++ WIP/Sources/WingS/WingS/WingS/Controllers/WebApi/DonationDoneController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API_NganLuong;
+using WingS.DataAccess;
+using WingS.DataHelper;
+using WingS.Models;
+using WingS.Models.DTOs;
+
+namespace WingS.Controllers
+{
+    public class DonationDoneController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult AddNewDonation()
+        {
+            String Token = Request["token"];
+            RequestCheckOrder info = new RequestCheckOrder();
+            info.Merchant_id = "24338";
+            info.Merchant_password = "12345612";
+            info.Token = Token;
+            APICheckoutV3 objNLChecout = new APICheckoutV3();
+            ResponseCheckOrder result = objNLChecout.GetTransactionDetail(info);
+            using (var db = new DonationDAL())
+            {
+                
+            }
+            return Ok(new HTTPMessageDTO { Status = WsConstant.HttpMessageType.SUCCESS, Data = 1 });
+        }
+    }
+}
