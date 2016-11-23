760f13c335c7a9c46226fe3f564393bdde12c3bb
nghiadtse03220 <nghiadtse03220@fpt.edu.vn>
2016-11-23 18:59:15 +0700

    add missing file EventypeDTO

---
+++ WIP/Sources/WingS/WingS/WingS/Models/DTOs/EventTypeDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WingS.Models.DTOs
+{
+    public class EventTypeDTO
+    {
+        public int EventTypeID { get; set; }
+        public string EventTypeName { get; set; }
+        public string Content { get; set; }
+        public int NumberRelatedEvent { get; set; }
+        public bool IsActive { get; set; }
+
+        public EventTypeDTO()
+        {
+            EventTypeID = 0;
+            EventTypeName = "";
+            Content = "";
+            NumberRelatedEvent = 0;
+            IsActive = false;
+        }
+    }
+}
