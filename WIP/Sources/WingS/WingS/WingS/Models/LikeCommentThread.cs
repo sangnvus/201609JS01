7083b7375ec2963b5b102b006bfb2294692fdb7a
nhienlhse03377 <blacksnow055@gmail.com>
2016-11-29 23:10:08 -0800

    Update db

---
+++ WIP/Sources/WingS/WingS/WingS/Models/LikeCommentThread.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WingS.Models
+{
+    public class LikeCommentThread
+    {
+        [Key]
+        public int LikeCommentId { get; set; }
+        [ForeignKey("Comment")]
+        public int CommentId { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public bool Status { get; set; }
+        public virtual CommentThread Comment { get; set; }
+        public virtual Ws_User User { get; set; }
+    }
+}
