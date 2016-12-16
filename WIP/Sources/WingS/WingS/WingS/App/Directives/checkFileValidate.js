7ec3d42df553b1aa6f2574e936e48ce70953ca53
tuanhase03108 <anhtuanvt93@gmail.com>
2016-12-16 23:32:11 +0700

    fix checkfile upload

--- WIP/Sources/WingS/WingS/WingS/App/Directives/checkFileValidate.js
+++ WIP/Sources/WingS/WingS/WingS/App/Directives/checkFileValidate.js
@@ -1,4 +1,4 @@
-﻿app.directive('validFile', function () {
+﻿app.directive('validFileEvent', function () {
 var validFormats = ['image/jpg', 'image/gif','image/jpeg','image/png','image/bmp'];
     return {
         require: 'ngModel',
@@ -15,6 +15,35 @@ var validFormats = ['image/jpg', 'image/gif','image/jpeg','image/png','image/bmp
                     }
                     if (validFormats.indexOf(thisFile[i].type.toLowerCase()) === -1) {
                         $scope.CreateEventForm.FileUpload.$setValidity("validFileType", false);
+                        break;
+                    }
+                }
+                $scope.$apply(function () {
+                    ngModel.$setViewValue(el.val());
+                    ngModel.$render();
+                });
+            });
+        }
+    }
+});
+
+app.directive('validFileEditEvent', function () {
+    var validFormats = ['image/jpg', 'image/gif', 'image/jpeg', 'image/png', 'image/bmp'];
+    return {
+        require: 'ngModel',
+        link: function ($scope, el, attrs, ngModel) {
+            //change event is fired when file is selected
+            el.bind('change', function () {
+                var thisFile = this.files;
+                $scope.EditEvent.FileUpload.$setValidity("validFileSize", true);
+                $scope.EditEvent.FileUpload.$setValidity("validFileType", true);
+                for (var i = 0; i < thisFile.length; i++) {
+                    if (thisFile[i].size > 20000000) {
+                        $scope.EditEvent.FileUpload.$setValidity("validFileSize", false);
+                        break;
+                    }
+                    if (validFormats.indexOf(thisFile[i].type.toLowerCase()) === -1) {
+                        $scope.EditEvent.FileUpload.$setValidity("validFileType", false);
                         break;
                     }
                 }
@@ -44,6 +73,35 @@ app.directive('validFileThread', function () {
                     }
                     if (validFormats.indexOf(thisFile[i].type.toLowerCase()) === -1) {
                         $scope.CreateThread.Images.$setValidity("validFileType", false);
+                        break;
+                    }
+                }
+                $scope.$apply(function () {
+                    ngModel.$setViewValue(el.val());
+                    ngModel.$render();
+                });
+            });
+        }
+    }
+});
+
+app.directive('validFileOrgani', function () {
+    var validFormats = ['image/jpg', 'image/gif', 'image/jpeg', 'image/png', 'image/bmp'];
+    return {
+        require: 'ngModel',
+        link: function ($scope, el, attrs, ngModel) {
+            //change event is fired when file is selected
+            el.bind('change', function () {
+                var thisFile = this.files;
+                $scope.CreateOrganization.LogoImage.$setValidity("validFileSize", true);
+                $scope.CreateOrganization.LogoImage.$setValidity("validFileType", true);
+                for (var i = 0; i < thisFile.length; i++) {
+                    if (thisFile[i].size > 20000000) {
+                        $scope.CreateOrganization.LogoImage.$setValidity("validFileSize", false);
+                        break;
+                    }
+                    if (validFormats.indexOf(thisFile[i].type.toLowerCase()) === -1) {
+                        $scope.CreateOrganization.LogoImage.$setValidity("validFileType", false);
                         break;
                     }
                 }
